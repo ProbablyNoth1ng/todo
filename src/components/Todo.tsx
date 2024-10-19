198a24efc2b5e9a973d6a5e2442cf6131bb2960b
MRnektodotpng <android.kaplan@gmail.com>
2024-10-19 20:46:09 +0200

    added task fold

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,25 +1,30 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import '../styles/Todo.scss';
 
 interface Task{
     id:number,
-    task_text:string,
-    completed:boolean
+    task_title:string,
+    task_description:string,
+    completed:boolean,
+    folded:boolean
 }
 
 export default function Todo(){
     const [tasks, setTasks] = useState<Task[]>([])
     const [newTask, setNewTask] = useState("")
+    const [newTaskDescription,setNewTaskDescription] = useState("");
 
     const addTask = (e:React.FormEvent) =>{
         e.preventDefault()
         if(newTask.trim()){
-            setTasks([...tasks,{id:Date.now(),task_text:newTask,completed:false}])
+            setTasks([...tasks,{id:Date.now(),task_title:newTask,task_description:newTaskDescription,completed:false,folded:true}])
             setNewTask("")
         }
         console.log(tasks)
     }
-    
+    const foldTask = (id:number) =>{
+        setTasks(tasks.map(task => task.id === id ? {...task,folded:!task.folded} : task))
+    }
     const deleteTask = (id:number) =>{
         setTasks(tasks.filter(task => task.id !== id))
     }   
@@ -27,6 +32,11 @@ export default function Todo(){
     const doneTask = (id:number) => {
         setTasks(tasks.map(task => task.id === id ? {...task,completed: !task.completed} : task))
     }
+    useEffect(() =>{
+        console.log(tasks.map(task => {
+            console.log(task.folded)
+        }))
+    },[tasks])
     return (
         <>
             <div className="todo py-20">
@@ -42,18 +52,33 @@ export default function Todo(){
                 </form>
                 <ul className="flex  flex-col items-center pt-5  ">
                     {tasks.map(task =>(
-                        <li className="text-white items-center flex  justify-between task">
-                            <span className=""> 
-
+                        
+                        <li className="text-white items-center task" >
+                            <div className=" flex  justify-between">
+                                <span className=""> 
+                       
                                 <input type="checkbox" 
                                         onClick={() => doneTask(task.id)} 
                                         checked={task.completed}
                                         />
-                                <span className={`${task.completed ? 'line-through' : ''  } pl-2`}>{task.task_text}</span> 
-                            </span> 
-                            
-                            <span onClick={() => deleteTask(task.id)}><i className="fa-regular fa-trash-can cursor-pointer "></i></span>
-                            </li>
+                                <span className={`${task.completed ? 'line-through' : ''  } pl-2`}>{task.task_title}</span> 
+                                </span> 
+                        <span>
+                            <span onClick={() => deleteTask(task.id)}><i className="fa-regular fa-trash-can cursor-pointer mr-2 "></i></span>
+                            <span onClick={() => foldTask(task.id)}>{task.folded ? <i className="fa-solid fa-chevron-up"></i> : <i className="fa-solid fa-chevron-down"></i> }</span>
+                        </span>
+                               
+                            </div>
+                           
+                            <div className={`task_description ${task.folded ? "hidden" : ''} `}>
+                                    <textarea 
+
+                                    onChange={(e) => setNewTaskDescription(e.target.value)}
+                                    placeholder="Enter a new description"
+                                    className="textarea rounded-lg p-5 bg-gray-100 text-white w-full mt-2 text-black"
+                                    />
+                            </div>
+                        </li>
                         
                     ))   }
                 </ul>
